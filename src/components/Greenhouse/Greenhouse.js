52196c87c312bc0c3d4a5f5d1c20b552087d74db
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-05-10 19:37:56 -0700

    phase 2 and 3

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,17 +1,26 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import { useEffect, useState } from 'react';
+import { useTheme } from '../../context/ThemeContext';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
+  const { themeName, setThemeName } = useTheme();
+  const [imgSrc, setImgsrc ] = useState(dayImage);
+
+  useEffect(()=> {
+    if(themeName === "Day") setImgsrc(dayImage);
+    if(themeName === "Night") setImgsrc(nightImage);
+  }, [themeName]);
 
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
+            src={imgSrc}
+            alt='greenhouse'
       />
       <LightSwitch />
       <ClimateStats />
--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,15 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext';
+import { useEffect } from 'react';
 
 function LightSwitch() {
+  const { themeName, setThemeName } = useTheme();
+
+  // useEffect(()=> console.log(themeName), [themeName]);
   return (
     <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+      <div className="on" onClick={ ()=> setThemeName("Day")}>DAY</div>
+      <div className="off" onClick={ () => setThemeName("Night")}>NIGHT</div>
     </div>
   );
 }
